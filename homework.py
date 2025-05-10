7989b8c63972ff0b0b4c127fe3646be9d151d9f2
Kylee Grumbling <kayladawngrumbling17@gmail.com>
2025-05-10 02:36:24 -0400

    added correct py file

--- homework.py
+++ homework.py
@@ -1,15 +1,26 @@
-# Step 1: Initialize an Empty List
-favorite_books = []
+#  1. Even or odd Creates a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
-# Step 2 & 3: Prompt the User for Input and Store the Books in the List
-for i in range(1, 4):
-    book = input(f"Enter the title of your favorite book #{i}: ")
-    favorite_books.append(book)
 
-# Step 4: Sort the List
-favorite_books.sort()
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+    
 
-# Step 5: Print the Sorted List
-print("\nYour favorite books in alphabetical order:")
-for book in favorite_books:
-    print(book)
+# 2. Convert a Number to a String this is a  function that can transform a number (integer) into a string.
+def number_to_string(num):
+    return str(num)
+
+# 3. Remove String Spaces this is a function that removes the spaces from the string, then return the resultant string.
+def no_space(x):
+    return x.replace(" ", "")
+
+# 4.Vowel Count this Returns the number (count) of vowels in the given string.
+def getCount(string):
+    vowels = "aeiou"
+    count = 0
+    for char in string:
+        if char in vowels:
+            count += 1
+    return count
